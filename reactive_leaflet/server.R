f08990136bc5ca956a9e81f5f39bb8707dbf7602
spymark <smarketos@gmail.com>
2016-11-24 14:54:08 +0000

    mvp

--- reactive_leaflet/server.R
+++ reactive_leaflet/server.R
@@ -8,24 +8,33 @@ server <- function(input, output, session) {
 
   points <- eventReactive(input$recalc, {
     
-    df <- data.frame(lat = runif(50, min = 55.95, max = 55.96), lng = runif(50, min = -3.19, max = -3.15))
+    df <- data.frame(lat = runif(5000, min = 55.95, max = 55.96), lng = runif(5000, min = -3.19, max = -3.15))
     
   }, ignoreNULL = FALSE)
   
   
   price <- eventReactive(input$recalc, {
-    
-    as.character(round(runif(50, min = 350, max = 780 ), digits = 2))
+        (round(runif(5000, min = input$slider1[1], max = input$slider1[2] ), digits = 2))
   }, ignoreNULL = FALSE)
   
-  
+  #here we receive the map style selections
+  map_style <- reactive({input$radio})
+
 
   output$mymap <- renderLeaflet({
     leaflet() %>%
-      addProviderTiles("Stamen.TonerLite",
-        options = providerTileOptions(noWrap = TRUE)
-      ) %>%
-      addMarkers(data = points(), popup = price())
+      addProviderTiles( map_style(),options = providerTileOptions(noWrap = TRUE)) %>%
+      addMarkers(data = points(), clusterOptions = markerClusterOptions(), popup = as.character(price()))
     
   })
+  
+  output$plot <- renderPlotly({
+    dat <- price()
+    dat <- data.frame(dat)
+    p <- ggplot(data = dat, aes(x=dat, fill = ..count..))+geom_histogram()+ggtitle("Distribution of prices")
+    ggplotly(p)
+    
+  })
+  
+  
 }
--- reactive_leaflet/ui.R
+++ reactive_leaflet/ui.R
@@ -11,11 +11,16 @@ ui <- fluidPage(
   sidebarLayout(
     
     sidebarPanel(
-      actionButton("recalc", "Recalculate Locations and Prices")
-    ),
+      radioButtons("radio", label = h3("Choose Map Style"),
+                   choices = list("Stamen.TonerLite", "OpenTopoMap" ,"OpenStreetMap.Mapnik"), 
+                   selected = "Stamen.TonerLite"),
+      sliderInput("slider1", label = h3("Define the price range"), min = 450, max = 1500, value = c(600, 850)),
+      actionButton("recalc", "Generate new data")
+          ),
     
     mainPanel(
-      leafletOutput("mymap")
+      leafletOutput("mymap"),
+      plotlyOutput("plot")
     )
   )
 )
