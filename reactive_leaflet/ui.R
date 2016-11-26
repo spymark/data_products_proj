cc1de2d9355ebadf6883e15afc643181b2fb0d3c
spymark <smarketos@gmail.com>
2016-11-26 20:18:49 +0000

    added more documentation, added extra maps

--- reactive_leaflet/ui.R
+++ reactive_leaflet/ui.R
@@ -13,7 +13,7 @@ ui <- fluidPage(
     sidebarPanel(
       
       radioButtons("radio", label = h3("Choose Map Style"),
-                   choices = list("Stamen.TonerLite", "OpenTopoMap" ,"OpenStreetMap.Mapnik"), 
+                   choices = list("Stamen.TonerLite", "OpenStreetMap.DE", "OpenStreetMap.HOT", "OpenStreetMap.Mapnik"), 
                    selected = "Stamen.TonerLite"),
       sliderInput("slider1", label = h3("Define the price range"), min = 450, max = 1500, value = c(600, 850)),
       sliderInput("slider2", label = h3("Number of markers"), min = 1000, max = 10000, value = 5000),
