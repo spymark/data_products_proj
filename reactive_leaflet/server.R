51298ae8231140783a5c17f626746549c4b3c306
spymark <smarketos@gmail.com>
2016-11-26 12:32:58 +0000

    added slider for number of markers

--- reactive_leaflet/server.R
+++ reactive_leaflet/server.R
@@ -6,15 +6,20 @@ library(plotly)
 
 server <- function(input, output, session) {
 
+  n <- eventReactive(input$recalc, {
+    input$slider2
+  })
+  
   points <- eventReactive(input$recalc, {
     
-    df <- data.frame(lat = runif(5000, min = 55.95, max = 55.96), lng = runif(5000, min = -3.19, max = -3.15))
+    df <- data.frame(lat = runif(input$slider2, min = 55.95, max = 55.96), lng = runif(input$slider2, min = -3.19, max = -3.15))
     
   }, ignoreNULL = FALSE)
   
   
   price <- eventReactive(input$recalc, {
-        (round(runif(5000, min = input$slider1[1], max = input$slider1[2] ), digits = 2))
+        
+        (round(runif(input$slider2, min = input$slider1[1], max = input$slider1[2] ), digits = 2))
   }, ignoreNULL = FALSE)
   
   #here we receive the map style selections
--- reactive_leaflet/ui.R
+++ reactive_leaflet/ui.R
@@ -6,16 +6,20 @@ library(plotly)
 
 ui <- fluidPage(
   
-  titlePanel("Simple map"),
+  titlePanel("Shiny reactive map"),
   
   sidebarLayout(
     
     sidebarPanel(
+      h3("Documentation"),
+      # h2("")
       radioButtons("radio", label = h3("Choose Map Style"),
                    choices = list("Stamen.TonerLite", "OpenTopoMap" ,"OpenStreetMap.Mapnik"), 
                    selected = "Stamen.TonerLite"),
       sliderInput("slider1", label = h3("Define the price range"), min = 450, max = 1500, value = c(600, 850)),
+      sliderInput("slider2", label = h3("Number of markers"), min = 1000, max = 10000, value = 5000),
       actionButton("recalc", "Generate new data")
+      
           ),
     
     mainPanel(
